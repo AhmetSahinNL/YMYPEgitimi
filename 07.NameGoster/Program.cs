66251ab1b18c4d20395c23d3cdc018cd0855cb23
ahmetsahinkg@gmail.com <ahmetsahinkg@gmail.com>
2023-07-18 19:59:33 +0300

    ,

--- 07.NameGoster/Program.cs
+++ 07.NameGoster/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Size hitap edebilmem için lütfen adınızı belirtiniz.");
             string name = Console.ReadLine();
             Console.WriteLine("Hoşgeldin " + name + " (-_-)");    
         }
